a3e6b2a871b84267d0be1f47cd038fb51759afb3
Kranti Kulkarni <kulkarnikranti12@gmail.com>
2025-04-16 08:21:28 +0530

    repo commit

---
+++ practice-coding/src/main/java/com/practice/coding/leetcode/array/KadensAlgo.java
@@ -0,0 +1,21 @@
+package com.practice.coding.leetcode.array;
+
+public class KadensAlgo {
+
+    public static void main(String[] args) {
+        int[] nums = {};//{5,4,-1,7,8};//{1};//{-2,1,-3,4,-1,2,1,-5,4};
+
+        int currSum =0, maxSum =Integer.MIN_VALUE ;
+        for(int i=0;i< nums.length;i++){
+            currSum = currSum + nums[i];
+
+            if(currSum > maxSum){
+                maxSum = currSum;
+            }
+            if(currSum < 0) {
+                currSum =0;
+            }
+        }
+        System.out.println(" max sum " + maxSum);
+    }
+}
---
+++ practice-coding/src/test/java/com/practice/coding/PracticeCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
